c4ed59b2066c214afdce8c5a3c985f6d3353c457
Malanaa <abdullahdotpy@gmail.com>
2024-03-29 13:50:53 -0700

    added transitioning img

--- main.js
+++ main.js
@@ -13,3 +13,14 @@ const observer = new IntersectionObserver((entries) => {
 hiddenElements.forEach((element) => {
   observer.observe(element);
 });
+
+window.addEventListener('scroll', function() {
+    var image = document.getElementById('syedimg');
+    var scrollPosition = window.scrollY;
+
+    // Adjust scale based on scroll position
+    var scale = 1 + scrollPosition * -0.0001;
+
+    // Apply the scale transformation
+    image.style.transform = 'scale(' + scale + ')';
+});
